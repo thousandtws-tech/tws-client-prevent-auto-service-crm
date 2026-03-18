d05552a2ca20319bfe425bcb1f2c504eceee1cd7
thousandtws-tech <vinicius@thousandtws.com>
2026-03-18 08:28:14 -0300

    fix

---
+++ apps/client/dist/assets/EmailOutlined-BhaUANVs.js
@@ -0,0 +1,1 @@
+import{c,j as t}from"./index-DJ97cQ_s.js";const m=c(t.jsx("path",{d:"M22 6c0-1.1-.9-2-2-2H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2zm-2 0-8 5-8-5zm0 12H4V8l8 5 8-5z"}),"EmailOutlined");export{m as E};
